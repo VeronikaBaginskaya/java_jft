877e1e0a68a5af6ce9e6d0dddaf4f48713173d50
Veronika <veronika_baginskaya@bk.ru>
2020-06-04 00:15:42 +0300

    3.10  if-else for creating group if don't exist

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,16 +5,13 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
 
     app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().groupCreate(new GroupData("test1", null, null));
 
   }
 
