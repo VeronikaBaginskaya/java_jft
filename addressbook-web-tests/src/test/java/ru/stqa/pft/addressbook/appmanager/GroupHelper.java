f365fd955c7e54ebf07c4ee7bcf0f622caadd5b5
Veronika <veronika_baginskaya@bk.ru>
2020-06-09 21:17:11 +0300

    4.5 choose element by number with list (last)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,9 +2,13 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupHelper extends  HelperBase {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupHelper extends HelperBase {
 
   public GroupHelper(WebDriver wd) {
     super(wd);
@@ -16,8 +20,8 @@ public class GroupHelper extends  HelperBase {
   }
 
   public void submitGroupCreation() {
-      click(By.name("submit"));
-    }
+    click(By.name("submit"));
+  }
 
   public void fillGroupForm(GroupData groupData) {
     type(By.name("group_name"), groupData.getName());
@@ -58,5 +62,16 @@ public class GroupHelper extends  HelperBase {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null );
+      groups.add(group);
+    }
+    return groups;
   }
 }
